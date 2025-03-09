3a454c4a28dbbc4a4d8da6af89ff8875755c37ad
Harshit77Nav <harshit77navneet@gmail.com>
2025-03-09 18:33:31 +0530

    power of four

--- BCA/Maths/PowerofFour.java
+++ BCA/Maths/PowerofFour.java
@@ -1,19 +1,14 @@
-// Power of Four
 package BCA.Maths;
 
-public class PowerofFour {
+public class PowerOfFour {
     public static void main(String[] args) {
-        // n = 16
-        int n = 16;
-        if((n&3) == 0) System.out.println(true);
-        else System.out.println(false);
-
-
-        /*  ..........................................................to extract k bits from the last
-         * int k = 4; // Number of bits to extract
-         * int mask = (1 << k) - 1; // Creates a mask with k bits set to 1
-         * int extracted = n & mask;
-         * System.out.println("Extracted Bits: " + extracted);
-         */
+        // n = 1
+        int n = 1;
+        if(n < 0) 
+            System.out.println(false);
+        if((n & (n-1))==0 && (n-1) % 3 == 0)
+            System.out.println(true);
+        else
+            System.out.println(false);
     }
 }
